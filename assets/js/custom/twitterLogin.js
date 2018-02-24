7ebafc158ad1b52416dc1ae58349c92fd7625a9a
Borja <borjagarciadelgado@hotmail.com>
2018-02-24 13:17:46 +0000

    Login de Twitter 2
    Login completo, salvo por añadir el localStorage para hacer el login permanente.

--- assets/js/custom/twitterLogin.js
+++ assets/js/custom/twitterLogin.js
@@ -1,3 +1,4 @@
+var backFromLogin = false;
 $(document).ready(function(){
     $('#logIn').on('click', function(){
         twitterLogin();
@@ -6,17 +7,12 @@ $(document).ready(function(){
     $('#logOut').on('click', function() {
         twitterLogout();
     });
+    alert("Documento Cargado");
     
-})
+    afterTwitterLogin();
+});
 
 /*
-    Lgin de Twitter casi terminado. Hay dos problemillas, uno más importante que el otro. Por un lado
-    la función getRedirectREsult debería ejecutarse después de signInWithRedirect pero no es así. Por 
-    alguna razón la redirección tarda un poco y el programa sigue leyendo y lee lo que hay debajo. Ya
-    me pasó algo parecido en una práctica de DOR y le encontré solución, epsero poder hacerlo aquí también
-    ya que la variable result nos trae los datos del usuario logueado y eso nos puede interesar. Por
-    supuesto si se os ocurre algo genial.
-
     Y por otro lado el otro detallito es que si se hace click para el login y en la página de 
     redirección se hace click en Cancelar se dan dos opciones para volver. Un botoncito y debajo un enlace
     El botoncito funciona perfectamente pero si se usa el enlace el botón de login no se vuelve a activar
@@ -25,22 +21,37 @@ $(document).ready(function(){
     que es 127.0.0.1.
 */
 function twitterLogin(){
+    backFromLogin = true;
     var provider = new firebase.auth.TwitterAuthProvider();
     firebase.auth().signInWithRedirect(provider);
+}
+
+function afterTwitterLogin(){
     firebase.auth().getRedirectResult().then(function(result) {
         if (result.credential) {
             // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
             // You can use these server side with your app's credentials to access the Twitter API.
+            
+            //Estas claves se usarían para otras peticiones a la API pero eso ya no nos hace falta
+            //En teoría el usuario ya se ha autentificado
             var token = result.credential.accessToken;
             var secret = result.credential.secret;
             // ...
-            
+            alert("Login Correcto con redirect");
+            // The signed-in user info.
+            var user = result.user;
+            var userName = user.displayName;
+            var avatar = user.photoURL;
+            alert(user.displayName + " | " +user.photoURL);
+            //A partir de aquí habría que hacer consistente el logging...probablemente guardar el nombre
+            //de usuario y su imagen en el localStorage porque hasta este punto si se recargfa la página
+            //es como si no estuviese logueado
+        }else{
+            /*Este alert solo es de prueba para comprobar que al entrar en la página
+            sin haberse logueado el flujo del programa va por aquí. El Else sobra y se puede quitar*/
+            alert("El usuario no ha hecho click en el botón de loggin");
         }
-        // The signed-in user info.
-        var user = result.user;
-        console.log(user);
-        alert("Login Correcto con redirect");
-        alert(user);
+        
     }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -58,6 +69,7 @@ function twitterLogout(){
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
         alert("Te has deslogueado con éxito");
+        //Aquí habría que borrar la variable del usuario en el localStorage
       }).catch(function(error) {
         // An error happened.
         alert("Error al desloguearse");
