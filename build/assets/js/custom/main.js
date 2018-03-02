0b97c2bd3ab5d98e7b83f55d85c49967a927b17c
Borja <borjagarciadelgado@hotmail.com>
2018-03-02 22:33:42 +0000

    eBay API Call que funciona
    Resubo la llamada a la API que funcionaba

--- build/assets/js/custom/main.js
+++ build/assets/js/custom/main.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 "use strict";
 
 // Código principal de la aplicación
 $(document).ready(function () {
     ebayApiCall();
 });
+
 function ebayApiCall() {
     var url = "http://svcs.ebay.com/services/search/FindingService/v1";
     url += "?OPERATION-NAME=findItemsByCategory";
@@ -24,46 +24,8 @@ function ebayApiCall() {
         url: url,
         dataType: "jsonp"
     });
-}
+};
 
 function findItemsByKeywordsCallback(json) {
     console.log(json);
-}
-=======
-'use strict';
-
-// Código principal de la aplicación
-$(document).ready(function () {
-    var eBayAPI = {
-        key: 'TipodeIn-AliBu-PRD-ddf6f4b63-4347d6bf',
-        requestURLBase: 'https://svcs.ebay.com/services/search/FindingService/v1?',
-        categories: {
-            watches: 31387,
-            tablets: 171485,
-            cameras: 31388
-        },
-        // lastResquest: [],
-        parseResponse: function parseResponse(r) {
-            var results = r.findItemsByCategoryResponse[0].searchResult[0].item;
-            for (var i = 0; i < results.length; i++) {
-                $(".products").append("<span>" + results[i].title + "</span>");
-            }
-        },
-        fetchData: function fetchData() {
-            var requestURL = this.requestURLBase + 'SECURITY-APPNAME=' + eBayAPI.key + '&OPERATION-NAME=' + 'findItemsByCategory' + '&SERVICE-VERSION=' + '1.0.0' + '&RESPONSE-DATA-FORMAT=' + 'JSON' + '&REST-PAYLOAD' + '&categoryId=' + this.categories.watches + '&paginationInput.entriesPerPage=' + '10' + '&GLOBAL-ID=' + 'EBAY-US' + '&siteid=' + '0';
-            return $.ajax({
-                url: requestURL,
-                type: 'GET',
-                dataType: 'JSONP',
-                context: this,
-                success: function success(r) {
-                    // this.lastResquest = [];
-                    this.parseResponse(r);
-                },
-                error: function error() {},
-                complete: function complete() {}
-            });
-        }
-    };
-});
->>>>>>> 76c4dfaf6d42c82365f9e590cebc521efd85f130
+};
--- source/assets/js/custom/main.js
+++ source/assets/js/custom/main.js
@@ -1,8 +1,8 @@
 // Código principal de la aplicación
-<<<<<<< HEAD
-$(document).ready(function(){
+$(document).ready(function() {
     ebayApiCall();
-})
+});
+
 function ebayApiCall(){
     var url = "http://svcs.ebay.com/services/search/FindingService/v1";
     url += "?OPERATION-NAME=findItemsByCategory";
@@ -22,52 +22,8 @@ function ebayApiCall(){
         url: url,
         dataType: "jsonp"
     })
-}
+};
 
 function findItemsByKeywordsCallback(json){
     console.log(json);
-}
-=======
-$(document).ready(function() {
-    var eBayAPI = {
-        key: 'TipodeIn-AliBu-PRD-ddf6f4b63-4347d6bf',
-        requestURLBase: 'https://svcs.ebay.com/services/search/FindingService/v1?',
-        categories: {
-            watches: 31387,
-            tablets: 171485,
-            cameras: 31388
-        },
-        // lastResquest: [],
-        parseResponse: function(r) {
-            var results = r.findItemsByCategoryResponse[0].searchResult[0].item;
-            for (var i = 0; i < results.length; i++) {
-                $(".products").append("<span>" + results[i].title + "</span>");
-            }
-        },
-        fetchData: function() {
-            var requestURL = this.requestURLBase +
-                'SECURITY-APPNAME=' + eBayAPI.key +
-                '&OPERATION-NAME=' + 'findItemsByCategory' +
-                '&SERVICE-VERSION=' + '1.0.0' +
-                '&RESPONSE-DATA-FORMAT=' + 'JSON' +
-                '&REST-PAYLOAD' +
-                '&categoryId=' + this.categories.watches +
-                '&paginationInput.entriesPerPage=' + '10' +
-                '&GLOBAL-ID=' + 'EBAY-US' +
-                '&siteid=' + '0';
-            return $.ajax({
-                url: requestURL,
-                type: 'GET',
-                dataType: 'JSONP',
-                context: this,
-                success: function(r) {
-                    // this.lastResquest = [];
-                    this.parseResponse(r);
-                },
-                error: function() {},
-                complete: function() {}
-            });
-        }
-    }
-});
->>>>>>> 76c4dfaf6d42c82365f9e590cebc521efd85f130
+};
