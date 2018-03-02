6a672719b10294b8bd86fb9ed5afa8627e1c76f0
Borja <borjagarciadelgado@hotmail.com>
2018-03-02 17:28:58 +0000

    Ebay API Call - Primera llamada
    Commit de la primera llamada satisfactoria a la API de Ebay

--- build/assets/js/custom/main.js
+++ build/assets/js/custom/main.js
@@ -1,2 +1,30 @@
-// Código principal de la aplicación
 "use strict";
+
+// Código principal de la aplicación
+$(document).ready(function () {
+    ebayApiCall();
+});
+function ebayApiCall() {
+    var url = "http://svcs.ebay.com/services/search/FindingService/v1";
+    url += "?OPERATION-NAME=findItemsByCategory";
+    url += "&SERVICE-VERSION=1.0.0";
+    url += "&SECURITY-APPNAME=TipodeIn-AliBu-PRD-ddf6f4b63-4347d6bf";
+    url += "&GLOBAL-ID=EBAY-ES";
+    url += "&RESPONSE-DATA-FORMAT=JSON";
+    url += "&callback=findItemsByKeywordsCallback";
+    url += "&REST-PAYLOAD";
+    url += "&categoryId(0)=31387";
+    url += "&categoryId(1)=31388";
+    url += "&paginationInput.pageNumber=1";
+    url += "&paginationInput.entriesPerPage=10";
+    console.log(url);
+    $.ajax({
+        method: "GET",
+        url: url,
+        dataType: "jsonp"
+    });
+}
+
+function findItemsByKeywordsCallback(json) {
+    console.log(json);
+}
